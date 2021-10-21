7fd64579e690bb500d1b02e196d3a839056d8fac
Vedantmagare <92874562+Vedantmagare@users.noreply.github.com>
2021-10-21 10:22:44 +0530

    Update Starter file.py

--- Starter file.py
+++ Starter file.py
@@ -1,13 +1,18 @@
+#some basic import
+
 from kivymd.app import MDApp
 from kivy.uix.image import Image
 from kivymd.uix.screen import MDScreen
 
-class img(MDApp):
+
+# class
+class App(MDApp):
     def build(self):
+        # app screen
         screen = MDScreen()
 
         return screen
 
 
 if __name__ == '__main__':
-    img().run()
+    App().run()
